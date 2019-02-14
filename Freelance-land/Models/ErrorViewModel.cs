242969d58b03f0a8768f3fc2206f6e5577c29228
Taras Karpliuk <taras.karpliuk@gmail.com>
2019-02-14 11:59:42 +0200

    Created project

---
+++ Freelance-land/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Freelance_land.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
